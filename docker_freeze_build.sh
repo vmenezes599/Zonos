799947d844907f4456e5294bf9d207d525ca0e5a
Vitor <vmenezes599@gmail.com>
2025-12-10 21:06:59 +0100

    Renamed image.

--- docker_freeze_build.sh
+++ docker_freeze_build.sh
@@ -5,5 +5,5 @@ VERSION=$(cat "$(dirname "$0")/docker_freeze_version")
 # Build zonos_server prod image
 DOCKER_BUILDKIT=1 docker build \
   --target zonos_server \
-  -t ghcr.io/vmenezes599/zonos_server:${VERSION} \
+  -t ghcr.io/vmenezes599/ct_zonos_server:${VERSION} \
   .
